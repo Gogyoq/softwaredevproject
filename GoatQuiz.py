b4df5dfa54852aed75fc9e55d6b66b821431a623
Gogyoq <81204088+Gogyoq@users.noreply.github.com>
2021-11-26 16:52:27 -0500

    Add files via upload
    Added animation when start button is clicked

--- GoatQuiz.py
+++ GoatQuiz.py
@@ -18,19 +18,20 @@ blue = (0,0,255)
 black = (0,0,0)
 navy = (21,76,121)
 
-#Pygame window is loaded here
+#Pygame window is loaded hereS
 #Along with width height and mouse variables
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Goat Quiz")
 width = screen.get_width()
 height = screen.get_height()
+startClicked = False
 
 #Sprites are loaded here
-startButtonImg = pygame.image.load(r'T:\BDH-ICS3C1-1\aida4421\Python\GoatTest\Sprites\StartButton.png')
+startButtonImg = pygame.image.load(r'C:\Users\gamer\Desktop\Genie\Python Project\Sprites\StartButton.png')
 startButtonImg = pygame.transform.scale(startButtonImg, (272,96))
-goatQuizTitle = pygame.image.load(r'T:\BDH-ICS3C1-1\aida4421\Python\GoatTest\Sprites\GoatQuizTitle.png')
+goatQuizTitle = pygame.image.load(r'C:\Users\gamer\Desktop\Genie\Python Project\Sprites\GoatQuizTitle.png')
 goatQuizTitle = pygame.transform.scale(goatQuizTitle, (544, 512))
-startButtonDownImg = pygame.image.load(r'T:\BDH-ICS3C1-1\aida4421\Python\GoatTest\Sprites\StartButtonDown.png')
+startButtonDownImg = pygame.image.load(r'C:\Users\gamer\Desktop\Genie\Python Project\Sprites\StartButtonDown.png')
 startButtonDownImg = pygame.transform.scale(startButtonDownImg, (272,96))
 #button class
 class Button():
@@ -59,25 +60,61 @@ class Button():
 #creating button instances
 startButton = Button(384,570,startButtonImg)
 
-
+clock = pygame.time.Clock()
 #Start of game loop
+mainMenu = True
+while(mainMenu == True):
+    
+    screen.fill(navy)
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  
+            pygame.quit()
+    
+    if startButton.draw() == False:
+        startButton = Button(384,570,startButtonImg)
+        screen.blit(goatQuizTitle,(240, 64))
+
+    else:
+        print("Start")
+        startButton = Button(384,570,startButtonDownImg)
+        startClicked = True
+        goatQuizSlide = 240
+        startButtonSlide = 384
+        while True:
+            
+            screen.fill(navy)
+            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:  
+                    False
+                    
+            goatQuizSlide = goatQuizSlide + 8
+            screen.blit(goatQuizTitle,(goatQuizSlide, 64))
+            
+            startButtonSlide = startButtonSlide - 8
+            screen.blit(startButtonImg,(startButtonSlide, 570))
+            
+            if goatQuizSlide > 1050:
+                mainMenu = False
+                break
+            
+            clock.tick(60)
+            pygame.display.flip()
+    
+    clock.tick(60)
+    pygame.display.flip()
+
 running = True
-while(running == True):
+while running == True:
     
     screen.fill(navy)
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  
             running = False
-    
-    if startButton.draw() == True:
-        print("Start")
-        startButton = Button(384,570,startButtonDownImg)
-    else:
-        startButton = Button(384,570,startButtonImg)
-    screen.blit(goatQuizTitle,(240, 64))
-    
-    pygame.display.update()
+            
+    clock.tick(60)
+    pygame.display.flip()
 
 pygame.quit()
-    

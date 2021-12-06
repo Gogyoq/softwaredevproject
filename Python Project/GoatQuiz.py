7dc7dbc40ab2891f971f01f68f079662016c08a2
Gogyoq <81204088+Gogyoq@users.noreply.github.com>
2021-12-05 21:16:57 -0500

    Add files via upload

---
+++ Python Project/GoatQuiz.py
@@ -0,0 +1,173 @@
+#Author: Aidan Kooiman
+#Date: 11/26/2021
+#FileName: GoatQuiz
+#Description: A personality quiz that assigns you one of twelve goats at the end
+
+import pygame
+
+import os
+import sys
+import webbrowser
+from time import sleep
+
+pygame.init()
+pygame.font.init()
+
+#Various Variables used for the pygame window are declared here
+size = width, height = 1024, 768
+center = (256,384)
+white = (255,255,255)
+gray = (211,211,211)
+darkGray = (105,105,105)
+blue = (0,0,255)
+black = (0,0,0)
+navy = (21,76,121)
+defFont = pygame.font.Font(r'C:\Users\gamer\Desktop\Genie\Python Project\Fonts\munro.ttf', 60)
+defFontQuestion = pygame.font.Font(r'C:\Users\gamer\Desktop\Genie\Python Project\Fonts\munro.ttf', 60)
+
+#Pygame window is loaded hereS
+#Along with width height and mouse variables
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Goat Quiz")
+width = screen.get_width()
+height = screen.get_height()
+startClicked = False
+
+#Getting current directory
+cwd = os.getcwd()
+print(cwd)
+#Sprites are loaded here
+#main menu stuff is loaded here
+startButtonImg = pygame.image.load(os.path.join(sys.path[0], r"Sprites\StartButton.png"), "r")
+startButtonImg = pygame.transform.scale(startButtonImg, (272,96))
+goatQuizTitle = pygame.image.load(os.path.join(sys.path[0], r"Sprites\GoatQuizTitle.png"), "r")
+goatQuizTitle = pygame.transform.scale(goatQuizTitle, (544, 512))
+startButtonDownImg = pygame.image.load(os.path.join(sys.path[0], r"Sprites\StartButtonDown.png"), "r")
+startButtonDownImg = pygame.transform.scale(startButtonDownImg, (272,96))
+#quiz buttons are loaded here
+button1 = pygame.image.load(os.path.join(sys.path[0], r"Sprites\Button1.png"), "r")
+button1 = pygame.transform.scale(button1,(80,80))
+button2 = pygame.image.load(os.path.join(sys.path[0], r"Sprites\Button2.png"), "r")
+button2 = pygame.transform.scale(button2,(80,80))
+button3 = pygame.image.load(os.path.join(sys.path[0], r"Sprites\Button3.png"), "r")
+button3 = pygame.transform.scale(button3,(80,80))
+button4 = pygame.image.load(os.path.join(sys.path[0], r"Sprites\Button4.png"), "r")
+button4 = pygame.transform.scale(button4,(80,80))
+button5 = pygame.image.load(os.path.join(sys.path[0], r"Sprites\Button5.png"), "r")
+button5 = pygame.transform.scale(button5,(80,80))
+button6 = pygame.image.load(os.path.join(sys.path[0], r"Sprites\Button6.png"), "r")
+button6 = pygame.transform.scale(button6,(80,80))
+#Pressed down version of each button
+button1Down = pygame.image.load(os.path.join(sys.path[0], r"Sprites\Button1Down.png"), "r")
+button1Down = pygame.transform.scale(button1Down,(80,80))
+button2Down = pygame.image.load(os.path.join(sys.path[0], r"Sprites\Button2Down.png"), "r")
+button2Down = pygame.transform.scale(button2Down,(80,80))
+button3Down = pygame.image.load(os.path.join(sys.path[0], r"Sprites\Button3Down.png"), "r")
+button3Down = pygame.transform.scale(button3Down,(80,80))
+button4Down = pygame.image.load(os.path.join(sys.path[0], r"Sprites\Button4Down.png"), "r")
+button4Down = pygame.transform.scale(button4Down,(80,80))
+button5Down = pygame.image.load(os.path.join(sys.path[0], r"Sprites\Button5Down.png"), "r")
+button5Down = pygame.transform.scale(button5Down,(80,80))
+button6Down = pygame.image.load(os.path.join(sys.path[0], r"Sprites\Button6Down.png"), "r")
+button6Down= pygame.transform.scale(button6Down,(80,80))
+
+#button class
+class Button():
+    def __init__(self, x, y, image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x , y)
+        self.clicked = False
+    
+    def draw(self):
+        action = False
+        #get mouse position
+        pos = pygame.mouse.get_pos()
+        #check if mouse is on button
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                action = True
+            if pygame.mouse.get_pressed()[0] == 0:
+                self.clicked = False
+        #draw button on screen
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+        
+        return action
+
+#creating button instances
+startButton = Button(384,570,startButtonImg)
+
+clock = pygame.time.Clock()
+#Start of game loop
+mainMenu = True
+while(mainMenu == True):
+    
+    screen.fill(navy)
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  
+            pygame.quit()
+    
+    if startButton.draw() == False:
+        screen.blit(goatQuizTitle,(240, 64))
+
+    else:
+        print("Start")
+        screen.fill(navy)
+        screen.blit(goatQuizTitle,(240, 64))
+        screen.blit(startButtonDownImg,(384,570))
+        pygame.display.flip()
+        sleep(.1)
+        screen.blit(startButtonImg,(384,570))
+        pygame.display.flip()
+        sleep(.1)
+        startClicked = True
+        goatQuizSlide = 240
+        startButtonSlide = 384
+        while True:
+            
+            screen.fill(navy)
+            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:  
+                    False
+                    
+            goatQuizSlide = goatQuizSlide + 8
+            screen.blit(goatQuizTitle,(goatQuizSlide, 64))
+            
+            startButtonSlide = startButtonSlide - 8
+            screen.blit(startButtonImg,(startButtonSlide, 570))
+            
+            if goatQuizSlide > 1050:
+                mainMenu = False
+                break
+            
+            clock.tick(60)
+            pygame.display.flip()
+    
+    clock.tick(60)
+    pygame.display.flip()
+
+text = defFontQuestion.render("Describe your personality with a food", False, white)
+running = True
+while running == True:
+    
+    screen.fill(navy)
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  
+            running = False
+    
+    screen.blit(text,(50, 50))
+    screen.blit(button1,(50,200))
+    screen.blit(button2,(50,290))
+    screen.blit(button3,(50,380))
+    screen.blit(button4,(50,470))
+    screen.blit(button5,(50,560))
+    screen.blit(button6,(50,650))
+            
+    clock.tick(60)
+    pygame.display.flip()
+
+pygame.quit()
